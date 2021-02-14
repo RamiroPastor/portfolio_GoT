d43aad8063a484f6aca3dffad2e8bcaf5afde02d
Cristóbal Martínez <wipeoman@gmail.com>
2021-02-14 01:26:26 +0100

    fighting with css and losing

--- src/assets/i18n/en.js
+++ src/assets/i18n/en.js
@@ -1,7 +1,14 @@
 export const en =
-  { "characters": "characters"
-  , "houses":     "houses"
-  , "chronology": "chronology"
-  , "goBack":     "go back"
-  , "search":     "search"
-  }
+    {
+        "characters": "characters"
+        , "houses": "houses"
+        , "chronology": "chronology"
+        , "goBack": "go back"
+        , "search": "search"
+        , "house": "house"
+        , "allegiances": "allegiances"
+        , "appearances": "appearances"
+        , "father": "father"
+        , "siblings": "siblings"
+        , "titles": "titles"
+    }
--- src/assets/i18n/es.js
+++ src/assets/i18n/es.js
@@ -1,7 +1,14 @@
 export const es =
-  { "characters": "personajes"
-  , "houses":     "casas"
-  , "chronology": "cronologia"
-  , "goBack":     "volver"
-  , "search":     "buscar"
-  }
+    {
+        "characters": "personajes"
+        , "houses": "casas"
+        , "chronology": "cronologia"
+        , "goBack": "volver"
+        , "search": "buscar"
+        , "house": "casa"
+        , "allegiances": "alianzas"
+        , "appearances": "apariciones"
+        , "father": "padre"
+        , "siblings": "hermanos"
+        , "titles": "titulos"
+    }
