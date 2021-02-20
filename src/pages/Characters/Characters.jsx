14b551c971538eaca13da7408c3b6cbabfde2ea6
nievesth <mary.nieves@bootcamp-upgrade.com>
2021-02-20 22:05:26 +0100

    'bien'

--- src/pages/Characters/Characters.jsx
+++ src/pages/Characters/Characters.jsx
@@ -1,45 +1,38 @@
 import React, {useState, useEffect} from "react";
 import './_Characters.scss';
-import { Footer } from "../../core/Footer/Footer";
-import { Header } from "../../core/Header/Header";
-import { useParams} from "react-router-dom";
+import {Footer} from "../../core/Footer/Footer";
+import {Header} from "../../core/Header/Header";
+import {useParams} from "react-router-dom";
 import {API} from '../../shared/const/api.const';
 
 export function Characters(props) {
-
-  const {characterName} = useParams();
-
-  const [character, setCharacter] = useState([]);
-    
+    const {characterName} = useParams();
+    const [character, setCharacter] = useState([]);
     const getCharacter = () => {
         API.get('show/characters/').then((res) => {
             setCharacter(res.data);
         });
-      }
-        useEffect(getCharacter, []);
+    }
+    useEffect(getCharacter, []);
+    return (
+        <div className="hero">
+            <Header
+                showGoBackButton={false}
+                showSearchBar={true}
+                showGoHomeButton={true}
+                t={props.t}
+                fnSetLang={props.fnSetLang}
+            />
 
-   return(
-    <div className="hero">
-      <Header
-        showGoBackButton={false}
-        showSearchBar={true}
-        showGoHomeButton={true}
-        t={props.t}
-        fnSetLang={props.fnSetLang}
-      />
-      {character.map((char, i) =>{
-        <div className={"c-character__top-info"}>
-        <img className={"c-character__image"} src={char.image} alt={char.name}/>
-        <h2>{char.name}</h2>
+            {character.map((char, i) => <div key={i} className={"c-character__top-info"}>
+                <img className={"c-character__image"} src={char.image} alt={char.name}/>
+                <h2>{char.name}</h2>
+            </div>)}
+
+            <Footer
+                t={props.t}
+            />
         </div>
-      } 
-      )}
-
-        
-      <Footer
-        t={props.t}
-      />
-</div>
-  )
+    )
 }
   
