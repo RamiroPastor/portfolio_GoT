1ca2d4a94e3844396bbc62ce413354b9f1328b75
Gerald Andrei Tamasi <tamasigerald@gmail.com>
2021-02-22 13:38:38 +0100

    fixed some errors

--- src/pages/Characters/Characters.jsx
+++ src/pages/Characters/Characters.jsx
@@ -23,7 +23,7 @@ export function Characters(props) {
         <div className="hero hero--char">
             <SimpleBar autoHide={false}>
             <div className="hero__container">
-                {character.map((char, i) => 
+                {characters.map((char, i) => 
                 <figure key={i} className={"hero__card"}>
                     <Link to={`/characters/${char.name}`}>
                         <img className="hero__card__img" src={char.image ? char.image : "https://i.imgur.com/p8G3kF0.png"} alt={char.name}/>
